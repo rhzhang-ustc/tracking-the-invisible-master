ed08265174dc41c18c94b1a6a824a493cf798074
Ruohan Zhang <82691357+Alouette18@users.noreply.github.com>
2022-07-03 08:57:54 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -7,11 +7,17 @@ import operator
 from functools import reduce
 
 # super params
-theta_match = 120  # for dim=128 features
-sigma_0 = 10000
+theta_match = 0.95  # for dim=128 features
+sigma_0 = 10
 theta_move = 0
 smoothing_alpha = 0.7
 object_thresh = 500
+
+
+def normalize_feat(feat):
+    norm = np.linalg.norm(feat)
+    feat = feat/(norm + 1e-4)
+    return feat
 
 
 def add_gaussion_map(img, pt, mean_r, mean_phi, var):
@@ -25,13 +31,16 @@ def add_gaussion_map(img, pt, mean_r, mean_phi, var):
     distance_map = np.expand_dims(distance_map, axis=3)  # (480, 640, 2, 1)
 
     var_inverse = np.linalg.inv(var)
+    det = np.linalg.det(var)
+
     exponent = np.array([[np.dot(np.dot(distance_map[i, j].T, var_inverse), distance_map[i, j]) for j in range(w)]
                          for i in range(h)])
     exponent = np.squeeze(exponent)
 
-    img += 1/np.sqrt(np.linalg.det(var)) * np.exp(-0.5 * exponent)
+    img_addition = 1/np.sqrt(det) * np.exp(-0.5 * exponent)
+    img = img + img_addition
 
-    return
+    return np.max(img_addition)
 
 
 def object_compare(obj1, obj2):
@@ -57,12 +66,12 @@ def obj_move(bbox, bbox_init):
     return np.sum(np.abs(bbox[i] - bbox_init[i]) for i in range(4)) > theta_move
 
 
-def write_result(frame_lst, output_path):
+def write_result(frame_lst, output_path, colored):
 
     frame_shape = frame_lst[0].shape
     video_size = (frame_shape[1], frame_shape[0])
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    video = cv2.VideoWriter(output_path, fourcc, 30, video_size, True)
+    video = cv2.VideoWriter(output_path, fourcc, 30, video_size, colored)
 
     for frame in frame_lst:
         video.write(frame)
@@ -83,9 +92,12 @@ cap = cv2.VideoCapture("ethCup_input.WMV")
 ret, first_frame = cap.read()
 prev_gray = cv2.cvtColor(first_frame, cv2.COLOR_BGR2GRAY)
 
-# kp1 = ptrGFTT.detect(prev_gray, None)  # (198, ), kp1[0].pt = (x, y)
-# kp1, des1 = sift.compute(prev_gray, kp1)  # (198, 128)
-kp1, des1 = sift.detectAndCompute(prev_gray, None)
+dst = cv2.cornerHarris(prev_gray, 2, 3, 0.02)
+kp_mask = np.zeros_like(prev_gray)
+kp_mask[dst > 0.001 * dst.max()] = 1
+kp_mask = cv2.dilate(kp_mask, None)
+
+kp1, des1 = sift.detectAndCompute(prev_gray, mask=kp_mask.astype(np.uint8))
 
 # init object to track using RGB frames
 tracker = cv2.TrackerKCF_create()
@@ -97,12 +109,14 @@ object_track = prev_gray[bbox[0]:bbox[0]+bbox[2], bbox[1]:bbox[1]+bbox[3]].copy(
 result_lst = []
 
 # init database
-data = namedtuple('data_line', 'idx, vote_phi, vote_r, vote_sigma')  # index, descriptor, voting feature
-db = [data(idx=i, vote_r=to_polar(kp1[i].pt, bbox_prev_center)[0],
-           vote_phi=to_polar(kp1[i].pt, bbox_prev_center)[1], vote_sigma=sigma_0 * np.eye(2)) for i in range(len(kp1))]
-db_des = (des1 - np.expand_dims(des1.mean(axis=1), axis=1)) / np.expand_dims(des1.std(axis=1), axis=1)  # normalize
+db_size = len(kp1)
 
-db_size = len(db)
+des_db = [normalize_feat(des1[i]) for i in range(db_size)]
+r_db = [to_polar(kp1[i].pt, bbox_prev_center)[0] for i in range(db_size)]
+cov_db = [sigma_0 * np.eye(2) for i in range(db_size)]
+phi_db = [to_polar(kp1[i].pt, bbox_prev_center)[1] for i in range(db_size)]
+pt_db = [kp1[i].pt for i in range(db_size)]
+
 
 # while run
 while cap.isOpened():
@@ -113,15 +127,19 @@ while cap.isOpened():
 
     frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # kp = ptrGFTT.detect(frame_gray, None)
-    # kp, des = sift.compute(frame_gray, kp)
-    kp, des = sift.detectAndCompute(frame_gray, None)
+    dst = cv2.cornerHarris(frame_gray, 2, 3, 0.02)
+    kp_mask = np.zeros_like(frame_gray)
+    kp_mask[dst > 0.001 * dst.max()] = 1
+    kp_mask = cv2.dilate(kp_mask, None)
 
-    des = (des - np.expand_dims(des.mean(axis=1), axis=1)) / np.expand_dims(des.std(axis=1), axis=1)
+    kp, des = sift.detectAndCompute(frame_gray, mask=kp_mask.astype(np.uint8))
+
+    des = np.array([normalize_feat(feat) for feat in des])
     kp_num = len(kp)  # m
 
-    corr = np.matmul(db_des, des.T)  # (198, m) m is the size of kps in next frame
+    corr = np.matmul(des_db, des.T)  # (198, m) m is the size of kps in next frame
     corr_reduce = corr.max(axis=0)  # (m ,0)
+
     idx_match = corr.argmax(axis=0)
     match_eval = corr_reduce > theta_match
     idx_match = [idx_match[i] if match_eval[i] else -1 for i in range(kp_num)]  # -1 no feature matched
@@ -131,15 +149,11 @@ while cap.isOpened():
 
     diff = object_compare(object_track, object_detect)
 
-    # x, y, w, h = bbox
-    # cv2.rectangle(frame, (int(x), int(y)), (int(x + w), int(y + h)), (0, 255, 0), 2)
-    # cv2.putText(frame, str(diff), (5, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
-    # cv2.imshow("test", frame)
-    # cv2.waitKey(3)
-
     if diff < object_thresh:
         #  learn the model
+        print("model learning")
         bbox_center = (bbox[0] + bbox[2] // 2, bbox[1] + bbox[3] // 2)
+
         if obj_move(bbox, bbox_prev):
             for i in range(kp_num):
                 if idx_match[i] != -1:
@@ -148,23 +162,27 @@ while cap.isOpened():
                     pt = kp[i].pt
                     distance, phi = to_polar(pt, bbox_center)
 
-                    vote_r = smoothing_alpha * db[origin_idx].vote_r + (1-smoothing_alpha) * distance
-                    vote_phi = smoothing_alpha * db[origin_idx].vote_phi + (1-smoothing_alpha) * phi
+                    r_db[origin_idx] = vote_r \
+                        = smoothing_alpha * r_db[origin_idx] + (1-smoothing_alpha) * distance
+                    phi_db[origin_idx] = vote_phi \
+                        = smoothing_alpha * phi_db[origin_idx] + (1-smoothing_alpha) * phi
 
                     x_estimate = np.array([pt[0] + vote_r * math.cos(vote_phi), pt[1] + vote_r * math.sin(vote_phi)])
                     diff = np.expand_dims(x_estimate - np.array(bbox_center), axis=1)
                     sigma = np.dot(diff, diff.T)
 
-                    vote_sigma = smoothing_alpha * db[origin_idx].vote_sigma + (1-smoothing_alpha) * sigma
-
-                    db[origin_idx] = data(idx=origin_idx, vote_r=vote_r, vote_phi=vote_phi, vote_sigma=vote_sigma)
+                    cov_db[origin_idx] = smoothing_alpha * cov_db[origin_idx] + (1-smoothing_alpha) * sigma
 
                 else:
+
                     pt = kp[i].pt
                     distance, phi = to_polar(pt, bbox_center)
-                    db.append(data(idx=db_size, vote_phi=phi, vote_r=distance, vote_sigma=sigma_0 * np.eye(2)))
-                    db_des = np.insert(db_des, db_size, des[i], axis=0)
-                    db_size = db_size + 1
+
+                    des_db.append(des[i])
+                    r_db.append(distance)
+                    phi_db.append(phi)
+                    cov_db.append(sigma_0 * np.eye(2))
+                    pt_db.append(pt)
 
         bbox_prev = bbox
         x, y, w, h = bbox
@@ -181,23 +199,25 @@ while cap.isOpened():
             origin_idx = idx_match[i]
             if origin_idx != -1:
                 # use matched feature to estimate objects
-                add_gaussion_map(gauss_map, kp[i].pt, db[origin_idx].vote_r, db[origin_idx].vote_phi, db[origin_idx].vote_sigma)
 
-                # test
+                distance = r_db[origin_idx]
+                phi = phi_db[origin_idx]
+                cov = cov_db[origin_idx]
 
-                center_x = int(db[origin_idx].vote_r * math.cos(db[origin_idx].vote_phi) + kp[i].pt[0])
-                center_y = int(db[origin_idx].vote_r * math.sin(db[origin_idx].vote_phi) + kp[i].pt[1])
+                max_prob = add_gaussion_map(gauss_map, kp[i].pt, distance, phi, cov)
+
+                center_x = int(distance * math.cos(phi) + kp[i].pt[0])
+                center_y = int(distance * math.sin(phi) + kp[i].pt[1])
 
                 start_point = (int(kp[i].pt[0]), int(kp[i].pt[1]))
                 end_point = (center_x, center_y)
 
                 cv2.line(frame, start_point, end_point, (0, 0, 255))
-                cv2.rectangle(frame, start_point, (start_point[0]+3, start_point[1]+3), (0, 0, 255))
+                cv2.rectangle(frame, start_point, (start_point[0] + 3, start_point[1] + 3), (0, 0, 255))
                 cv2.rectangle(frame, end_point, (end_point[0] + 3, end_point[1] + 3), (255, 0, 0))
 
         maximum = np.unravel_index(np.argmax(gauss_map), gauss_map.shape)
-        cv2.rectangle(frame, maximum, (maximum[0]+3, maximum[1]+3), (0, 255, 0), 2)
+        cv2.rectangle(frame, maximum, (maximum[0] + 3, maximum[1] + 3), (0, 255, 0), 2)
         result_lst.append(frame)
 
-    write_result(result_lst, "test_4.mp4")  # indent not right
-
+    write_result(result_lst, "test.mp4", True)  # indent not right
